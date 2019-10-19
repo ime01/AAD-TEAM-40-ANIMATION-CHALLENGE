8b5a920140fd028ab740574099274e10ea3fec72
FLOWZ <ketuimevbore@yahoo.com>
2019-10-19 12:37:31 -0700

    This is an example of how the quiz/questions will appear

--- app/src/main/java/com/flowz/kidsplayground/QuizActivity.java
+++ app/src/main/java/com/flowz/kidsplayground/QuizActivity.java
@@ -1,15 +1,24 @@
 package com.flowz.kidsplayground;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Next = (Button)findViewById(R.id.next);
+
+        openPreviousFragment(Next);
     }
 
     @Override
@@ -17,4 +26,21 @@ public class QuizActivity extends AppCompatActivity {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
+
+    public void openNewFragment(View view) {
+        BFragment fragment = new BFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_container, fragment, getString(R.string.b));
+        transaction.commit();
+
+    }
+
+    public void openPreviousFragment(View view) {
+        AFragment fragment = new AFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_container, fragment, getString(R.string.b));
+        transaction.commit();
+
+    }
+
 }
